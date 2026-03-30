ea28a0015de732fb9ef8906724e2f8884e40f95c
lenda deif <lendadeif277@gmail.com>
2026-03-30 04:52:34 +0200

    edit req

--- IF_model.py
+++ IF_model.py
@@ -1,6 +1,3 @@
-import insightface
-from insightface.app import FaceAnalysis
-
-app = FaceAnalysis(name="buffalo_l")   
-app.prepare(ctx_id=-1)                 
-print("InsightFace models downloaded!")
+import os
+for file in os.listdir("captures"):
+    os.remove(os.path.join("captures", file))
